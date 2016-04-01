6d5d88cdd28fa2ecd0c4c2d13941dc47af51b226
Daniel Sarfati <danielrsarfati@gmail.com>
2016-04-01 08:30:41 -0700

    Initial commit of basic project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using GrainInterfaces1;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1 : Orleans.TestingHost.TestingSiloHost
+    {
+        [Fact]
+        public async void TestMethod1()
+        {
+            const long id = 0;
+
+            var grain = GrainFactory.GetGrain<IGrain1>(id);
+
+            // This will create and call a Hello grain with specified 'id' in one of the test silos.
+            var reply = await grain.SayHello();
+
+        }
+    }
+}
